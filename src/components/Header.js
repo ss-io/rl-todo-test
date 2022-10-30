db0559c10a0d80450064e784d0dce724562cd345
Sam.S <ss.io.code@gmail.com>
2022-10-30 22:03:59 +1100

    add task info and general code clean up

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,19 @@ const Header = () => {
   return (
     <div className="page-header-wrapper">
       <div className="page-header">
+        <h1>Your task</h1>
+        <p>
+          You have been asked to develop a to-do list application that allows the user to create and manage tasks. The application has the following features:
+        </p>
+        <ul>
+          <li>Add and view tasks</li>
+          <li>Delete a task</li>
+          <li>Complete a task</li>
+          <li>Set a priority for my tasks</li>
+          <li>View the tasks sorted by priority and name</li>
+          <li>View the number of total and completed tasks</li>
+        </ul>
+        <div className="divider"></div>
       </div>
     </div>
   )
